a789091f812e784e60fb572b03584a17374f4d69
ESI-SYD <1505632943@qq.com>
2022-08-15 09:53:28 +0800

    temp branch syd_new exclude auto_generated directorys from ESI-SYD (#2)
    * add analyze_perf_logs
    
    * Update models.py
    
    * replace specific paths
    
    * revert change
    
    * revert change
    
    * revert change
    
    Co-authored-by: Weizhuo Zhang <weizhuo.zhang@intel.com>

--- test_generated_models.sh
+++ test_generated_models.sh
@@ -1,44 +1,55 @@
 #!/bin/bash
-cur_dir=`pwd`
-mkdir -p ${cur_dir}/models/
-mkdir -p ${cur_dir}/temp/
-mkdir -p ${cur_dir}/shapes/
-mkdir -p ${cur_dir}/submodules/
+CORES=`lscpu | grep Core | awk '{print $4}'`
+SOCKETS=`lscpu | grep Socket | awk '{print $2}'`
+TOTAL_CORES=`expr $CORES \* $SOCKETS - 1`
+benchmark_dir=`pwd`
+
+rm -rf ${benchmark_dir}/submodules/
+mkdir -p ${benchmark_dir}/submodules/
+
 python rewrite_models.py
 
-dir=${cur_dir}/models/
+dir="${benchmark_dir}/models/"
 for item in `ls $dir`
 do
     echo $item
     python $dir$item
 done
 
-dir=${cur_dir}/temp/
-result_dir=${cur_dir}/shapes/
+dir="${benchmark_dir}/temp/"
+result_dir="${benchmark_dir}/shapes/"
 for item in `ls $dir`
 do
     echo $item
     name=`echo $item | awk -F'.' '{printf $1}'`
-    python $dir$item | tee ${result_dir}${name}_1_3_224_224
+    python $dir$item | tee ${result_dir}${name}_BS_3_224_224
 done
 
 python auto_gen.py
-dir=${cur_dir}/submodules/
+dir="${benchmark_dir}/submodules/"
 for len in `ls $dir`
 do
     for pattern in `ls $dir/$len`
     do
+        timestamp=`date +%Y%m%d_%H%M%S`
+        perf_file_name=${pattern}_perf_${timestamp}.txt        
+        core_cnt=-1
+        echo "${pattern}" | tee $perf_file_name
         for item in `ls $dir/$len/$pattern`
         do
+            if [[ core_cnt -eq $TOTAL_CORES ]]; then
+                core_cnt=-1
+        	    wait
+            fi
             if [[ $item == *".py" ]]; then
-                echo $item
-                python $dir/$len/$pattern/$item
+                ((core_cnt++)); numactl --physcpubind ${core_cnt} python $dir/$len/$pattern/$item | tee -a $perf_file_name &
             fi
         done
-        # if [ -f $dir/$len/$pattern/all.txt ];
-        # then
-        #     continue
-        # fi
+        wait
+        if [ -f $dir/$len/$pattern/all.txt ];
+        then
+            continue
+        fi
         python analyze_submodules.py --dir $dir/$len/$pattern
     done
 done
